c6ccb19eddc7aca86dfc7aeb6b2f543c8c19f81d
bison-- <mike.bison42@googlemail.com>
2021-09-25 19:55:08 +0200

    cleaning test

--- main.c
+++ main.c
@@ -5,19 +5,14 @@
 #define NUMBER_OF_OPTIONS 2
 
 void print2Dchar(const char **, int rows);
-void print2DcharP(const char *ar);
-int getRowsFromConst2Dchar(const char **ar);
 
 int main() {
     const char *options[NUMBER_OF_OPTIONS] = {
-            "1) wheeee 1",
-            "2) wheeee 2",
-            //NULL
+        "1) wheeee 1",
+        "2) wheeee 2"
     };
-    //printf("%d", NUMBER_OF_OPTIONS);
-    //printf("%d\n", strlen(options));
+
     print2Dchar(options, NUMBER_OF_OPTIONS);
-    //print2DcharP(options);
     printf("Select: ");
 
     int selected = 0;
@@ -41,26 +36,6 @@ int main() {
 void print2Dchar(const char **ar, int rows) {
     for (int i = 0; i < rows; i ++) {
         printf("%s", ar[i]);
-        printf("\n");
-    }
-}
-
-int getRowsFromConst2Dchar(const char **ar) {
-    // 'total' will be 70 = 10 * 7
-    int total = sizeof(*ar);
-
-    // 'column' will be 7 = size of first row
-    int column = sizeof((*ar)[0]);
-
-    // 'row' will be 10 = 70 / 7
-    int rows = total / column;
-    return rows;
-}
-
-void print2DcharP(const char *ar) {
-    int number = sizeof(ar) / sizeof(ar[0]);
-    for (int i = 0; i < number; i ++) {
-        printf("%s", &(ar[i]));
         printf("\n");
     }
 }
