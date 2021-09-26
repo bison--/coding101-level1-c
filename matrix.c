f1dee6f5fba74598b4974a91b95a167e76e6f47d
bison-- <mike.bison42@googlemail.com>
2021-09-26 09:49:23 +0200

    added MATRIX and shuffled helper functions

---
+++ matrix.c
@@ -0,0 +1,53 @@
+//
+// Created by Gamer on 26.09.2021.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "matrix.h"
+#include "helper.h"
+
+void drawMatrix() {
+    int rows;
+    int cols;
+    int randomAmount;
+
+    printf("\nWidth: ");
+    scanf_s("%d", &cols);
+
+    printf("\nHeight: ");
+    scanf_s("%d", &rows);
+
+    printf("\nRandoms: ");
+    scanf_s("%d", &randomAmount);
+
+    int fieldsTotal = rows * cols;
+    if (randomAmount > fieldsTotal) {
+        printf("\nERROR: more randoms than fields!");
+        return;
+    }
+
+    int *randomIndexes;
+    randomIndexes = (int *)calloc(randomAmount, sizeof(int));
+
+    fillArrayWithValue(randomIndexes, fieldsTotal, -1);
+    for (int i = 0; i < randomAmount; i++) {
+        int newRandom = getRandomRange(0, fieldsTotal);
+        while (arrayHasValue(randomIndexes, fieldsTotal, newRandom)) {
+            newRandom = getRandomRange(0, fieldsTotal);
+        }
+        randomIndexes[i] = newRandom;
+    }
+
+    for (int i = 0; i < fieldsTotal; i++) {
+        if (arrayHasValue(randomIndexes, fieldsTotal, i)) {
+            printf("*");
+        } else {
+            printf("#");
+        }
+
+        if ((i + 1) % cols == 0) {
+            printf("\n");
+        }
+    }
+}
---
+++ matrix.h
@@ -0,0 +1,10 @@
+//
+// Created by Gamer on 26.09.2021.
+//
+
+#ifndef CODING101_MATRIX_H
+#define CODING101_MATRIX_H
+
+void drawMatrix();
+
+#endif //CODING101_MATRIX_H
